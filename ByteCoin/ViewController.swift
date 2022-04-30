0bff63e3af5651cbfcd99a24e830aeeeef2abf97
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2022-04-30 18:56:49 +0500

    Initial commit

---
+++ ByteCoin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ByteCoin
+//
+//  Created by Anton Vikhlyaev on 30.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
