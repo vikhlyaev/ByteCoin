c42b276a266b0bbb7488a71f2921829e11f396f2
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2022-05-01 14:49:18 +0500

    Adds an IBOutlets

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -8,7 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var cryptoLogo: UIImageView!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var cryptoPicker: UIPickerView!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
